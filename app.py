c4d2390ab55607aa3198253f4f81fd078aac5ea4
Makoto Kogoh <neapoli.smith@gmail.com>
2021-10-17 15:41:03 +0900

    Updated word list api.

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import gensim
 from sklearn.decomposition import PCA
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['JSON_AS_ASCII'] = False
 
+MAX_WORD_LIST = 10
+
 model = gensim.models.Word2Vec.load("./data/ja.bin")
+word_list = model.wv.index2word
 
 
 @app.route("/word2vec/words", methods=["GET"])
 def words():
-    data = {"words": model.wv.index2word}
+    if request.args.get("q") is not None:
+        filtered = list(filter(lambda word: request.args.get("q") in word, word_list))
+        data = {"words": filtered[:MAX_WORD_LIST]}
+    else:
+        data = {"words": word_list[:MAX_WORD_LIST]}
+
     return jsonify(data)
 
 
@@ -19,7 +29,7 @@ def words():
 def word2vec():
     if request.args.get("words") is None:
         return jsonify({"message": "words is not specified"}), 400
-    
+
     words = request.args.get("words").split(",")
     vec_list = []
     for word in words:
@@ -27,21 +37,24 @@ def word2vec():
             return jsonify({"message": "{} is not defined".format(word)}), 400
         vec_list.append(model.wv[word])
 
+    if len(words) == 1:
+        vec_list.append(model.wv[words[0]])
+
     pca = PCA(n_components=2)
     pca.fit(vec_list)
     vec_list_pca = pca.transform(vec_list)
-    
+
     result = []
     for i in range(len(words)):
         word = words[i]
-        vec = vec_list[i]
+        # vec = vec_list[i]
         vec_pca = vec_list_pca[i]
         result.append({
             "word": word,
             # "vec": vec.tolist(),
             "pca": vec_pca.tolist()
         })
-    
+
     return jsonify({"code": 0, "result": result})
 
 
@@ -50,14 +63,14 @@ def similar():
     # pos, neg
     if request.args.get("pos") is None and request.args.get("neg") is None:
         return jsonify({"message": "pos/neg is not specified"}), 400
-    
+
     vec_list = []
-    
+
     if request.args.get("pos") is None:
         pos = []
     else:
         pos = request.args.get("pos").split(",")
-    
+
     if request.args.get("neg") is None:
         neg = []
     else:
@@ -74,7 +87,7 @@ def similar():
             return jsonify({"message": "{} is not defined".format(word)}), 400
         else:
             vec_list.append(model.wv[word])
-    
+
     sims = model.wv.most_similar(positive=pos, negative=neg)
     for sim in sims:
         vec_list.append(model.wv[sim[0]])
@@ -102,7 +115,7 @@ def similar():
             "pca": vec_list_pca[index].tolist()
         })
         index += 1
-    
+
     for sim in sims:
         result.append({
             "word": sim[0],
@@ -111,8 +124,12 @@ def similar():
             "pca": vec_list_pca[index].tolist()
         })
         index += 1
-    
-    return jsonify({"code": 1, "pos": input_pos, "neg": input_neg, "result": result})
+
+    return jsonify({
+        "code": 1,
+        "pos": input_pos,
+        "neg": input_neg,
+        "result": result})
 
 
 if __name__ == "__main__":
